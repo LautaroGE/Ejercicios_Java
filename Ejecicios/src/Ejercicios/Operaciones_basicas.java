ebc9d48cfebdb5f7fac1282c19faaceb8f36d648
Lauta <Lauta@DESKTOP-GPV4M8U>
2023-04-15 05:27:57 -0300

    update

--- Ejecicios/src/Ejercicios/Operaciones_basicas.java
+++ Ejecicios/src/Ejercicios/Operaciones_basicas.java
@@ -1,4 +1,5 @@
 package Ejercicios;
+import java.util.*;
 
 public class Operaciones_basicas {
 
@@ -6,6 +7,32 @@ public class Operaciones_basicas {
 		// Escribe un programa que le pida al usuario dos números y luego imprima en la consola el 
 		//resultado de la suma, resta, multiplicación y división de esos números...
 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero.");
+		int a = input.nextInt();
+		
+		System.out.println("El primer numero es: " + a);
+		
+		System.out.println("Ingrese otro numero.");
+		int b = input.nextInt();
+		
+		System.out.println("El segundo numero es: " + b);
+	
+		
+		int suma = a + b;
+		int resta = a - b;
+		int mult = a * b;
+		double div = (double) a / b;
+		
+		
+		System.out.println("La suma entre " + a + " y " + b + " es: " + suma);
+		System.out.println("La resta entre " + a + " y " + b + " es: " + resta);
+		System.out.println("La multiplicacion entre " + a + " y " + b + " es: " + mult);
+		System.out.printf("La division entre " + a + " y " + b + " es: " + "%.2f", div);
+		
+		
+		
 		
 		
 		
