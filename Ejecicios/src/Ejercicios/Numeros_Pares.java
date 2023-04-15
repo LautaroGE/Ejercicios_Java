c629b8cab02c56a176765cf8eb9e048cbed4e349
Lauta <Lauta@DESKTOP-GPV4M8U>
2023-04-15 04:57:58 -0300

    update

--- Ejecicios/src/Ejercicios/Numeros_Pares.java
+++ Ejecicios/src/Ejercicios/Numeros_Pares.java
@@ -6,7 +6,7 @@ public class Numeros_Pares {
 	public static void main(String[] args) {
 
 		//Escribe un programa que le pida al usuario un número y luego imprima en la consola 
-		//todos los números pares del 0 hasta ese número.
+		//todos los números pares del 0 hasta ese número
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Escriba un numero, por favor: ");
