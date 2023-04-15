46821abbfa0cef96661a1f92c53ed06c8f096720
Lauta <Lauta@DESKTOP-GPV4M8U>
2023-04-15 04:52:08 -0300

    update

--- Ejecicios/src/Ejercicios/Operaciones_basicas.java
+++ Ejecicios/src/Ejercicios/Operaciones_basicas.java
@@ -4,7 +4,7 @@ public class Operaciones_basicas {
 
 	public static void main(String[] args) {
 		// Escribe un programa que le pida al usuario dos números y luego imprima en la consola el 
-		//resultado de la suma, resta, multiplicación y división de esos números.
+		//resultado de la suma, resta, multiplicación y división de esos números...
 
 		
 		
